a7b9da856f8f03cd4f9d1c2aeeef1c43a7e105c4
Lavken <lavkenpilar@gmail.com>
2024-12-13 19:00:47 -0300

    create-pago-exitoso

--- src/component/Prueba.jsx
+++ src/component/Prueba.jsx
@@ -7,13 +7,13 @@ import ListGroup from 'react-bootstrap/ListGroup';
 const Prueba = () => {
   return (
     <Card style={{ width: '54rem' }}>
-    <Card.Img variant="top" src='/kaiak.svg' style={{width:'36rem'}}/>
-    <Card.Body style={{width:'54rem'}}>
+    <Card.Img variant="top" src='/2.svg' style={{width:'54rem'}}/>
+   {/* <Card.Body style={{width:'54rem'}}>
       <Card.Title style={{width:'54rem', color:'green'}} ><h2>Aprovecha esta oportunidad para hacer un gran regalo</h2></Card.Title>
       </Card.Body>
       <Card.Body>
       <Card.Link href='https://mpago.la/2coBZyo' style={{borderRadius:6, color:'blue'}}>Pincha aqui para Comprar</Card.Link>
-     </Card.Body>
+     </Card.Body>*/}
   </Card>
   )
 }
